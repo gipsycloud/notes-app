f675985c6a90259a8339f128c9a6f0a6231cfc42
webgipsy <babybird1801@gmail.com>
2025-02-06 11:52:09 +0630

    adding pagination in blog

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const { isLoggedIn } = require('../middleware/checkAuth');
 const dashboardController = require('../controllers/dashboardController');
 
+
 router.get('/dashboard', isLoggedIn, dashboardController.dashboard);
+router.get('/dashboard/:page?', isLoggedIn, dashboardController.dashboard);
 router.get('/dashboard/item/:id', isLoggedIn, dashboardController.item);
 
 router.get('/dashboard/add_note', isLoggedIn, dashboardController.addItem);
